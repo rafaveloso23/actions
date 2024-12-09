1b9c48dfdadb1d55cec02fd26592941a7f342f23
rafael <rafa.v_s@hotmail.com>
2024-12-09 13:42:52 -0300

    ad

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-hub" {
   name     = "rg-actions"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "rg-hub-dev" {
+  name     = "rg-actions-${var.environemnt}"
+  location = "eastus"
+}
--- variable.tf
+++ variable.tf
@@ -1,3 +1,3 @@
-# variable "azure_client_id" {
-#     type = string
-# }
+variable "environment" {
+    type = string
+}
