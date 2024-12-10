32f36eefb5c19aa4a366cbd0de974168470390c5
rafael <rafa.v_s@hotmail.com>
2024-12-10 19:31:19 -0300

    ad

--- providers.tf
+++ providers.tf
@@ -1,10 +1,3 @@
-# variable "oidc_token" {}
-# variable "oidc_token_file_path" {}
-# variable "oidc_request_token" {}
-# variable "oidc_request_url" {}
-
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -19,13 +12,8 @@ provider "azurerm" {
   features {}
 
   subscription_id = "cc323661-bdfb-4e37-8224-b9f41308d182"
-  client_id       = "5271f41f-e8ce-4cb2-aa1d-1eacae1d4eee"
+  client_id       = var.client_id
   use_oidc        = true
-
-  # # for GitHub Actions
-  # oidc_request_token = var.oidc_request_token
-  # oidc_request_url   = var.oidc_request_url
-
   tenant_id = "0eed3ea8-f35c-4862-b14a-9809318064c7"
 }
 
