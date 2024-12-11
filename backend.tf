04cfb19ed322b615a48c0df482a28ff13d3b7609
rafael <rafa.v_s@hotmail.com>
2024-12-11 14:30:48 -0300

    ad

--- backend.tf
+++ backend.tf
@@ -1,14 +1,3 @@
-    terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "veloso"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        # workspaces {
-        #   name = "stack-dev"
-        # }
-      }
-    }
 terraform {
   cloud {
     organization = "veloso"
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg-hub-dev" {
-  name     = "rg-actions-esst-${var.environment}"
+  name     = "rg-actions-t-${var.environment}"
   location = "eastus"
 }
