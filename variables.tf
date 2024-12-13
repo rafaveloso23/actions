9309f72237cf768c65e06515c24118691a77dd60
rafael <rafa.v_s@hotmail.com>
2024-12-13 10:31:08 -0300

    a

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "rg_existente" {
 variable "rg_name" {
   description = "Base name for the resource group"
   type        = string
-  default     = "rg-actions"
+  default     = "rg-actaions"
 
 #   validation {
 #     condition     = var.rg_existente == false || var.rg_name != ""
