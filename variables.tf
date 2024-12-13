a61f210937dd3ea1e8e8515e6be7aa92fd654d24
rafael <rafa.v_s@hotmail.com>
2024-12-13 09:02:42 -0300

    ad

--- variables.tf
+++ variables.tf
@@ -12,3 +12,32 @@ variable "workspaces" {
     type = string
     default = null
 }
+
+variable "rg_novo" {
+  description = "Indicates whether to create a new resource group"
+  type        = bool
+  default     = true
+}
+
+variable "rg_existente" {
+  description = "Indicates whether to use an existing resource group"
+  type        = bool
+  default     = false
+}
+
+variable "rg_name" {
+  description = "Base name for the resource group"
+  type        = string
+  default     = "rg-domain-rvs-dev"
+
+#   validation {
+#     condition     = var.rg_existente == false || var.rg_name != ""
+#     error_message = "When rg_existente is true, rg_name must not be empty."
+#   }
+}
+
+variable "rg_location" {
+  description = "Azure region for the resource group"
+  type        = string
+  default     = "eastus"
+}
