9b190285044d79e936430617667a196d970d317b
rafael <rafa.v_s@hotmail.com>
2024-12-16 11:12:39 -0300

    a

---
+++ contoso-01/variables.tf
@@ -0,0 +1,38 @@
+variable "environment" {
+    type = string
+    default = null
+}
+
+variable "client_id" {
+    type = string
+    default = null
+}
+
+variable "workspaces" {
+    type = string
+    default = null
+}
+
+# variable "rg_novo" {
+#   description = "Indicates whether to create a new resource group"
+#   type        = bool
+#   default     = true
+# }
+
+# variable "rg_existente" {
+#   description = "Indicates whether to use an existing resource group"
+#   type        = bool
+#   default     = false
+# }
+
+# variable "rg_name" {
+#   description = "Base name for the resource group"
+#   type        = string
+#   default     = "rg-actaions"
+# }
+
+# variable "rg_location" {
+#   description = "Azure region for the resource group"
+#   type        = string
+#   default     = "eastus"
+# }
