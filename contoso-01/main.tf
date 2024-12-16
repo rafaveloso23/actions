f0930681c1495fcf3bf128691c658ff4266158c0
rafael <rafa.v_s@hotmail.com>
2024-12-16 11:13:49 -0300

    a

--- contoso-01/main.tf
+++ contoso-01/main.tf
@@ -2,7 +2,7 @@ module "rg" {
   source = "git::https://github.com/rafaveloso23/tf-modules.git//rg?ref=main"
 
   # Add any necessary input variables for the module here
-  rg_name       = "examplses-rg"
+  rg_name       = "exampslses-rg"
   rg_location   = "eastus"
   environment   = var.environment
   rg_novo       = true
