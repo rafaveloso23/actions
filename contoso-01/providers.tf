a374acea7aaef73b937eafa42f90c9bf3e6aa192
rafael <rafa.v_s@hotmail.com>
2024-12-27 13:20:44 -0300

    ad

--- contoso-01/providers.tf
+++ contoso-01/providers.tf
@@ -5,17 +5,21 @@ terraform {
       version = "=4.1.0"
     }
   }
-  
+
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  features {
+    keyvault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 
   subscription_id = "cc323661-bdfb-4e37-8224-b9f41308d182"
   client_id       = var.client_id
   #client_id       = "5271f41f-e8ce-4cb2-aa1d-1eacae1d4eee"
-  use_oidc        = true
+  use_oidc  = true
   tenant_id = "0eed3ea8-f35c-4862-b14a-9809318064c7"
 }
 
