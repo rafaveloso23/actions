fdda0b704efb54deb9c8e1cd8041b474f7bbb8b9
rafael <rafa.v_s@hotmail.com>
2024-12-31 17:37:33 -0300

    a

--- contoso-01/import.tf
+++ contoso-01/import.tf
@@ -1,4 +1,4 @@
 import {
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${module.rg.resource_group_name}/providers/Microsoft.KeyVault/vaults/${var.kv_name}/objectId/${module.kv.current_object_id}"
-  to = module.kv.azurerm_key_vault_access_policy.current["default"]
+  to = module.kv.azurerm_key_vault_access_policy.current[0]
 }
