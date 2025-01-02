e487ff70da3dad674c5fd8e24df2725ca1790949
rafael <rafa.v_s@hotmail.com>
2025-01-02 12:04:05 -0300

    ad

--- contoso-01/import.tf
+++ contoso-01/import.tf
@@ -1,31 +1,31 @@
+
+#Import condition, will only import the resource if the object_id_found = true, before the first execution.
 import {
   for_each = local.execute_import ? { "default" = true } : {}
   to       = module.kv.azurerm_key_vault_access_policy.current["default"]
   id       = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${module.rg.resource_group_name}/providers/Microsoft.KeyVault/vaults/${var.kv_name}/objectId/${module.kv.current_object_id}"
 }
 
-
-data "azurerm_key_vault" "example" {
-  name                = module.kv.key_vault_name
-  resource_group_name = module.rg.resource_group_name
-}
-
-
 output "object_ids" {
-  value = [for policy in data.azurerm_key_vault.example.access_policy : policy.object_id]
+  value = [for policy in local.data_kv_access_policy : policy.object_id]
 }
 
 output "object_id_found" {
   value = contains(
-    [for policy in data.azurerm_key_vault.example.access_policy : policy.object_id],
+    [for policy in local.data_kv_access_policy : policy.object_id],
     local.specific_object_id
   )
 }
 
+# This locals will iterate over each access_policy entries from 'local.data_kv_access_policy';
+# The 'policy.object_id' extracts the object_id from each policy, the result should be like this '[ "object-id-1", "object-id-2" ]';
+# The second argument will validate the specific object id that we are looking for 'local.specific_object_id'
+# The contains() function checks if the list [ "object-id-1", "object-id-2" ] includes the value of local.specific_object_id, doing this we are able to
 locals {
+  data_kv_access_policy = module.kv.acces_policy
   specific_object_id = module.kv.current_object_id
   execute_import = contains(
-    [for policy in data.azurerm_key_vault.example.access_policy : policy.object_id],
+    [for policy in local.data_kv_access_policy : policy.object_id],
     local.specific_object_id
   )
 }
